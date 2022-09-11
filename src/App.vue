6897fc4709b582ff9d12fba1dee0a3baafddfd49
Vyacheslav <viparamount@gmail.com>
2022-09-11 16:01:00 +0300

    Added simple Vue.js blog component - now I can add new post

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,70 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+    <div class="app">
+        <form @submit.prevent>
+            <h4>Создание поста</h4>
+            <input v-bind:value="title" @input="title = $event.target.value" class="input" type="text" placeholder="Заголовок">
+            <input v-bind:value="body" @input="body = $event.target.value" class="input" type="text" placeholder="Текст">
+            <button @click="createPost">Добавить пост</button>
+        </form>
+        <div class="post" v-for="post in posts">
+            <div><strong>Название:</strong> {{ post.title }}</div>
+            <div><strong>Описание:</strong> {{ post.body }}</div>
+        </div>
+    </div>
 </template>
 
+<script>
+    export default {
+        data(){
+            return {
+                posts: [
+                    {
+                        id:1,
+                        title:'Javascript',
+                        body:'Описание поста 1'
+                    },
+                    {
+                        id:2,
+                        title:'Javascript',
+                        body:'Описание поста 2'
+                    },
+                    {
+                        id:3,
+                        title:'Javascript',
+                        body:'Описание поста 3'
+                    },
+
+                ],
+                title: '',
+                body: ''
+                
+            }
+        },
+        methods: {
+            createPost(event) {
+               
+                const newPost = {
+                    id: Date.now(),
+                    title: this.title,
+                    body: this.body,
+                }
+                this.posts.push(newPost);
+                    },
+            
+         }
+    }
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    }
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+.post {
+    padding: 15px;
+    border: 3px solid green;
+    margin-top: 20px;
 }
 </style>
